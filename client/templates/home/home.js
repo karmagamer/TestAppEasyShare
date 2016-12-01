06f2088df3cc15cd326a5212544f782d31cebe3c
karmagamer <karmapandya@Karmas-MacBook-Air.local>
2016-12-01 15:39:28 -0800

    my first commit

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,3 +1,30 @@
 Template.home.rendered = function() {
 
+
 };
+
+    // You can also put your ShareIt.configure here:
+    Template.home.helpers({
+    shareData: function() {
+      return {title: '#ThisIsATestt',
+      author: 'testEmail' ,
+      summary: 'This is a test',
+      url:'http://www.promunka.com/',
+    article: 'hi'
+    }
+
+  }
+});
+    ShareIt.configure({
+  sites: {
+    'facebook': {
+      'appId': 1598645950443803
+    }
+  },
+  useFB: true,
+  useTwitter: true,
+  useGoogle: false,
+  classes: "large btn",
+  iconOnly: true,
+  applyColors: true
+});
